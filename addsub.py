f2e6dfc68113bca6f895d6d8e100ff8c51a20cdc
vaibhaviprabhu-15 <vaibhaviprabhu707@gmail.com>
2025-10-16 12:50:23 +0530

    Create addsub.py

---
+++ addsub.py
@@ -0,0 +1,11 @@
+# Program: Addition and Subtraction of Two Numbers
+# Taking input from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# Performing addition
+sum_result = a + b
+# Performing subtraction
+sub_result = a - b
+# Display results
+print("Addition:", sum_result)
+print("Subtraction:", sub_result)
